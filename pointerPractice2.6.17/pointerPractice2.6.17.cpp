0a777ae2d4fa864d56ab15233da2298e8fa79aa5
JustinReed <reedjw2@vcu.edu>
2017-02-06 18:52:26 -0500

    pointerPractice1lll

--- pointerPractice2.6.17/pointerPractice2.6.17.cpp
+++ pointerPractice2.6.17/pointerPractice2.6.17.cpp
@@ -2,10 +2,23 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
+
 
 
 int main()
 {
+	int i = 10;
+	int *ptr1 = &i;
+
+
+
+	cout << i << endl;
+	cout << ptr1 << endl;
+	cout << &i << endl;
+	cout << &ptr1 << endl;
+
     return 0;
 }
 
